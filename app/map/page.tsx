2e7a7c0bf2dd2a54b80c80fd3c63d5d98b6e9b2f
Amadi Jerry <amadijerry310@gmail.com>
2025-09-08 11:20:01 +0100

    minor fix

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,16 +1,17 @@
-"use client"
-import AmalaMap from "@/components/map/AmalaMap";
+"use client";
+
+import dynamic from "next/dynamic";
 import Navbar from "@/components/map/Navbar";
 
-const Map = () => {
+const AmalaMap = dynamic(() => import("@/components/map/AmalaMap"), {
+  ssr: false,
+});
+
+export default function Map() {
   return (
     <div className="min-h-screen bg-background">
-      {/* Navigation */}
       <Navbar />
-
       <AmalaMap />
     </div>
   );
-};
-
-export default Map;
+}
